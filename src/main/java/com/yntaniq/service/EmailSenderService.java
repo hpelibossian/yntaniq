1bc7fc5c8a073a04a037f98c3abe37f2a0d8cba1
Hratchia PELIBOSSIAN <pelibossian@gmail.com>
2016-10-15 22:30:27 +0200

    email

---
+++ src/main/java/com/yntaniq/service/EmailSenderService.java
@@ -0,0 +1,34 @@
+package com.yntaniq.service;
+
+import com.yntaniq.mail.EmailHtmlSender;
+import com.yntaniq.mail.EmailSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+
+/**
+ * Created by pelibossian on 14/10/16.
+ */
+@Service
+public class EmailSenderService {
+    @Autowired
+    EmailHtmlSender emailHtmlSender;
+
+    @Autowired
+    EmailSender emailSender;
+
+
+    public EmailSenderService() {
+    }
+
+    @Scheduled(cron = "0 0 18 * * *")// (fixedDelay = 2500)// (cron = "0 15 10 15 * ?")"0 0 8-10 * * *"
+    public void systemSendEmail() {
+        com.yntaniq.mail.EmailStatus emailStatus = emailSender.sendPlainText("pelibossian@gmail.com","namak","namaki parunakutyun");
+
+        Context context = new Context();
+        context.setVariable("title", "Lorem Ipsum");
+        context.setVariable("description", "Lorem Lorem Lorem");
+        emailStatus = emailHtmlSender.send("pelibossian@gmail.com", "Title of email", "email/template-1", context);
+    }
+}
