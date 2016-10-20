baeaea2df33c27ee9dc508346544d6864d3785a8
Hratchia PELIBOSSIAN <pelibossian@gmail.com>
2016-10-20 22:34:06 +0200

    no message

--- src/main/java/com/yntaniq/elasticsearch/configuration/ElasticsearchConfiguration.java
+++ src/main/java/com/yntaniq/elasticsearch/configuration/ElasticsearchConfiguration.java
@@ -3,23 +3,14 @@ package com.yntaniq.elasticsearch.configuration;
 import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
 
 import java.net.UnknownHostException;
-import java.util.concurrent.TimeUnit;
 import java.net.InetAddress;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.client.Client;
-import org.elasticsearch.client.node.NodeClient;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.common.transport.TransportAddress;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-import javax.annotation.Resource;
 
 @Configuration
 public class ElasticsearchConfiguration {
